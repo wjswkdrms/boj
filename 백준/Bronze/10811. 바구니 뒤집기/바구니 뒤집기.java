757900a3db3f4f87641287c759d2ef2fca89d3d9
wjswkdrms <wjswkdrms7@gmail.com>
2023-12-14 14:39:36 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 144 ms, Memory: 15936 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] p = new int[N + 1];
+		for (int i = 0; i < p.length; i++) {
+			p[i] = i;
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			while(a < b) {
+				int temp = p[a];
+				p[a] = p[b];
+				p[b] = temp;
+				a++;
+				b--;
+			}
+		}
+		for (int i = 1; i < p.length; i++) {
+			bw.write(p[i] + " ");
+		}
+		br.close();
+		bw.close();
+	}
+}
