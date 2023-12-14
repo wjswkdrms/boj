6701ed202cf7f7b71d20072c3681abad5c855fa8
wjswkdrms <wjswkdrms7@gmail.com>
2023-12-14 10:48:28 +0900

    [Silver V] Title: 숫자 카드, Time: 1256 ms, Memory: 128676 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+		TreeSet<Integer> card = new TreeSet<>();
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			card.add(Integer.parseInt(st.nextToken()));
+		}
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			if (card.contains(Integer.parseInt(st.nextToken()))) {
+				bw.write("1 ");
+			} else {
+				bw.write("0 ");
+			}
+		}
+
+		br.close();
+		bw.close();
+	}
+}
