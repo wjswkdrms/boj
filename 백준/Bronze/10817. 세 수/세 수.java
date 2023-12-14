20f64932c66a9d7aba1cd98aefe22004738d9abb
wjswkdrms <wjswkdrms7@gmail.com>
2023-12-14 10:15:28 +0900

    [Bronze III] Title: 세 수, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int result = A + B + C - Math.max(A, Math.max(B, C)) - Math.min(A, Math.min(B, C));
+		bw.write(String.valueOf(result ));
+		br.close();
+		bw.close();
+	}
+}
