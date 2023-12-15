94f75ae8a93f777c400b664402b1523c781ae0c2
wjswkdrms <wjswkdrms7@gmail.com>
2023-12-15 13:47:13 +0900

    [Silver IV] Title: 돌 게임 2, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+		bw.write(Integer.parseInt(br.readLine())%2 == 0 ? "SK" : "CY");
+		
+		bw.close();
+		br.close();
+	}
+}
