64ffa4e9d5c962d52306c004576ab630e8180fbd
wjswkdrms <wjswkdrms7@gmail.com>
2023-12-15 09:49:57 +0900

    [Bronze II] Title: 커트라인, Time: 132 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		int[] p = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			p[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(p);
+		bw.write(String.valueOf(p[N-K]));
+		br.close();
+		bw.close();
+	}
+}
