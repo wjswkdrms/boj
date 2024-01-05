6fcba0e91a0638e981e7fe836886d9a30d001c33
wjswkdrms <wjswkdrms7@gmail.com>
2024-01-05 10:01:36 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 140 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				bw.write(" ");
+			}
+			for (int j = 0; j < (N - i) * 2 - 1; j++) {
+				bw.write("*");
+			}
+			bw.newLine();
+		}
+		for (int i = N - 2; i >= 0; i--) {
+			for (int j = 0; j < i; j++) {
+				bw.write(" ");
+			}
+			for (int j = 0; j < (N - i) * 2 - 1; j++) {
+				bw.write("*");
+			}
+			bw.newLine();
+		}
+		bw.close();
+		br.close();
+	}
+}
