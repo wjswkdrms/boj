699788d76667a60368d02283ab9a92d856d0ddfe
wjswkdrms <wjswkdrms7@gmail.com>
2024-01-08 17:33:04 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				bw.write("*");
+			}
+			bw.newLine();
+		}
+		bw.close();
+		br.close();
+    }
+}
