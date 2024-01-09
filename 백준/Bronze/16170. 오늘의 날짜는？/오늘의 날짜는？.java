a9801b7b4a54db9f1b9429c8c31db75e04c332c4
wjswkdrms <wjswkdrms7@gmail.com>
2024-01-09 15:22:51 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 120 ms, Memory: 14000 KB -BaekjoonHub

--- 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -3,16 +3,12 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Calendar;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
-		int year = Calendar.getInstance().get(Calendar.YEAR);
-		int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
-		int date = Calendar.getInstance().get(Calendar.DATE);
-		bw.write(year + "\n" + month + "\n" + date);
+		bw.write("2024\n1\n9");
 		bw.close();
 		br.close();
     }
