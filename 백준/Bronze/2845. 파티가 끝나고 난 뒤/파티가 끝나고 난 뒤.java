afe2726a8cf1141c5f9b7493c9c2ea4608fa6311
wjswkdrms <wjswkdrms7@gmail.com>
2024-01-23 15:40:13 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 156 ms, Memory: 14648 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 5; i++) {
+			bw.write((Integer.parseInt(st.nextToken())-(N*M)) + " " );
+		}
+		
+		bw.close();
+		br.close();
+	}
+}
