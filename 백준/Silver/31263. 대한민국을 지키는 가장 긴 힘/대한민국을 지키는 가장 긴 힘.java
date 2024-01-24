63bce286286e9610c6cb535ec0b75ee347abac00
wjswkdrms <wjswkdrms7@gmail.com>
2024-01-24 17:30:43 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 128 ms, Memory: 14236 KB, Score: 64 point -BaekjoonHub

---
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		char[] arr = br.readLine().toCharArray();
+		int count = 0;
+		int mem = 0;
+		for (int i = 0; i < N; i++) {
+			mem = mem * 10 + (arr[i]-'0');
+			if(mem > 641) {
+				count++;
+				mem = (arr[i] - '0');
+				if(mem == 0) {
+					mem = arr[i - 1] * 10;
+				}
+			}
+		}
+		bw.write(String.valueOf(count + 1));
+		bw.close();
+		br.close();
+	}
+}
